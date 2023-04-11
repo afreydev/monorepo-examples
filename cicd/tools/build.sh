04b98f7e8fc5a19ad982b9d4a4d165ec1182a362
Angel Rey <afreydev@gmail.com>
2023-04-11 12:02:33 -0500

    Added strings directly

--- cicd/tools/build.sh
+++ cicd/tools/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Building: $1 - Component: $2"
+echo "Building: $1 - Component: $2 - Path: $3"
--- cicd/tools/deploy.sh
+++ cicd/tools/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Deploying: $1 - Component: $2"
+echo "Deploying: $1 - Component: $2 - Path: $3"
